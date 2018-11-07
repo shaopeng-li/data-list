60283dda66686cc83f402705718bab431d8cf611
ShaopengLi <811131196@qq.com>
2018-11-07 00:19:49 -0600

    Updates

---
+++ precache-manifest.5f9c2e3883d2bb9a88dccca90b20339e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a4ad00662f7cdc8e786",
+    "url": "/data-list/static/js/runtime~main.3a4ad006.js"
+  },
+  {
+    "revision": "c66b6d6a3f669d5c4448",
+    "url": "/data-list/static/js/main.c66b6d6a.chunk.js"
+  },
+  {
+    "revision": "4cf46a97bf69aa500aef",
+    "url": "/data-list/static/js/1.4cf46a97.chunk.js"
+  },
+  {
+    "revision": "c66b6d6a3f669d5c4448",
+    "url": "/data-list/static/css/main.c03f9911.chunk.css"
+  },
+  {
+    "revision": "60ad2cc21a7d4923cd17564d3cd379a7",
+    "url": "/data-list/index.html"
+  }
+];
